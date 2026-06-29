7310e5f60352c8111526a4e10db5e81c00b15b05
CameraHub <camerahub@example.com>
2026-06-29 22:52:21 +0800

    fix: update base path

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  base: '/Camera-project/',
+  base: '/camera-project/',
   resolve: {
     alias: {
       '@': '/src'
